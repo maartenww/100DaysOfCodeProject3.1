7cb2416c49ef3ed9effae22a4592315f0727a9ac
Maarten <jachtschipwong@hotmail.com>
2018-09-16 17:12:21 +0200

    Added jumpin

--- sprites.py
+++ sprites.py
@@ -1,7 +1,9 @@
 import pygame as pg
+import pygame as pg
 from settings import *
 
-
+vec = pg.math.Vector2
+#TODO: ADD MOVING GROUND
 class Spritesheet():
 
     def __init__(self,filename):
@@ -11,49 +13,78 @@ class Spritesheet():
         image = pg.Surface((width,height))
         image.blit(self.spritesheet, (0, 0), (x, y, width, height))
         return image
-
+#TODO: Add obstacles
 class Ground(pg.sprite.Sprite):
-    x_pos = 0
-    y_pos = 900 / 1.30
-    ground_pos = (x_pos, y_pos)
     ground_sprite_width = 1600
-    ground_sprite_height = 19
+    ground_sprite_height = 13
     ground_sprite_size = (ground_sprite_width, ground_sprite_height)
-
-    x_pos1 = ground_sprite_width
-    y_pos1 = ground_sprite_height
 
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface(((self.ground_sprite_size)))
         self.image = pg.image.load_extended('sprites/ground.png').convert_alpha()
-        self.image = pg.transform.scale(self.image, (self.ground_sprite_width,19 ))
+        self.image = pg.transform.scale(self.image, (self.ground_sprite_width,13 ))
         self.rect = self.image.get_rect()
 
-        self.rect.x = self.x_pos
-        self.rect.y = self.y_pos
+        self.plat_pos_x = 0
+        self.plat_pos_y = 750
 
-        all_sprites.add(self)
+        self.rect.x = self.plat_pos_x
+        self.rect.y = self.plat_pos_y
+
+        self.REAL_Y = self.rect.y - self.ground_sprite_height
 
 class Player(pg.sprite.Sprite):
-    x_pos = 200
-    y_pos = 900 / 1.375
 
-    player_sprite_width = 73
-    player_sprite_height = 78
+    player_sprite_width = 87 #73
+    player_sprite_height = 94 #78
 
     def __init__(self):
 
         pg.sprite.Sprite.__init__(self)
-        self.image = Spritesheet.get_img(Spritesheet('sprites/dino.png'), 0, 0, 87,94)
+        self.image = Spritesheet.get_img(Spritesheet('sprites/dino.png'), 0, 0, 87,94) #94
         self.rect = self.image.get_rect()
         self.image.set_colorkey(BLACK)
         self.image = pg.transform.scale(self.image, (self.player_sprite_width, self.player_sprite_height))
 
-        self.rect.x = self.x_pos
-        self.rect.y = self.y_pos
+        self.player_pos = vec(SCREEN_WIDTH / 8, 657)#SCREEN_HEIGHT / 1.375 -100
 
-        all_sprites.add(self)
+        self.rect.x = self.player_pos.x
+        self.rect.y = self.player_pos.y
 
-    def pUpdate(self): #Todo: Add player movement (Jumping and also add obastacles)
-        pass
+
+        self.player_vel = vec(0, 0)
+
+        self.player_acc = vec(0, 0)
+
+        self.canjump = False
+
+    def jump(self):
+        if self.canjump == True:
+
+            self.player_vel.y = -10
+
+    def pUpdate(self):
+        self.player_acc = vec(0, PLAYER_GRAVITY)
+
+        self.player_vel.y += self.player_acc.y
+
+        self.player_pos.y += self.player_vel.y + 0.5 * self.player_acc.y
+
+        self.rect.y = self.player_pos.y
+
+
+        #print(str(self.player_vel.y))
+
+    def handle(self, event):
+        pressed = pg.key.get_pressed()
+        if pressed[pg.K_s]:
+            print('duck')
+
+        if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+            print('space')
+            self.jump()
+            self.canjump = False
+
+        elif event.type == pg.KEYUP and event.key == pg.K_SPACE:
+            self.player_acc.y = 0
